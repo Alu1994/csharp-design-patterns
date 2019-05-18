07f514d8714a365ce062250f7c8a8ad00a858d73
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2019-05-18 19:25:30 -0300

    Updating Adapter Pattern Module Adding the Bridge Pattern Module

---
+++ DesignPatters-7-Bridge/Example1/BridgeExample.cs
@@ -0,0 +1,93 @@
+﻿using Autofac;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace DesignPatters_7_Bridge.Example1
+{
+    public class BridgeExample
+    {
+        // Not using Dependency Injection
+        static void Mains(string[] args)
+        {
+            IRenderer renderer = new RasterRenderer();
+            //var renderer = new VectorRenderer();
+            var circle = new Circle(renderer, 5);
+            circle.Draw();
+            circle.Resize(2);
+            circle.Draw();
+        }
+
+        // Using Dependency Injection
+        static void Mains2(string[] args)
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<RasterRenderer>().As<IRenderer>().SingleInstance();
+            cb.Register((c, p) => new Circle(c.Resolve<IRenderer>(), p.Positional<float>(0)));
+
+            cb.RegisterType<VectorRenderer>().As<IRenderer>().SingleInstance();
+            cb.Register((c, p) => new Circle(c.Resolve<IRenderer>(), p.Positional<float>(0)));
+
+            using (var c = cb.Build())
+            {
+                var circle = c.Resolve<Circle>(new PositionalParameter(0, 5.0f));
+                circle.Draw();
+                circle.Resize(2.0f);
+                circle.Draw();
+            }
+        }
+    }
+
+    public interface IRenderer
+    {
+        void RenderCircle(float radius);
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing a circle of radius {radius}");
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing pixels for circle with radius {radius}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        protected Shape(IRenderer renderer) =>
+            this.renderer = renderer ?? throw new ArgumentNullException(paramName: nameof(renderer));
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+
+    public class Circle : Shape
+    {
+        private float radius;
+
+        public Circle(IRenderer renderer, float radius) : base(renderer)
+        {
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            renderer.RenderCircle(radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+}
---
+++ DesignPatters-7-Bridge/Exercise/Exercise.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using static System.Console;
+
+namespace DesignPatters_7_Bridge.Exercise
+{
+    public class Exercise
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(new Square(new VectorRenderer()));
+            WriteLine(new Triangle(new RasterRenderer()));
+        }
+    }
+
+    public interface IRenderer
+    {
+        string WhatToRenderAs { get; set; }
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        protected Shape(IRenderer renderer)
+        {
+            if (renderer == null)
+                throw new ArgumentNullException(paramName: nameof(renderer));
+
+            this.renderer = renderer;
+        }
+    }
+
+    public class Square : Shape
+    {
+        public Square(IRenderer renderer) : base(renderer)
+        {
+            renderer.WhatToRenderAs = "Square";
+        }
+
+        public override string ToString()
+        {
+            return renderer.ToString();
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle(IRenderer renderer) : base(renderer)
+        {
+            renderer.WhatToRenderAs = "Triangle";
+        }
+
+        public override string ToString()
+        {
+            return renderer.ToString();
+        }
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public string WhatToRenderAs { get; set; }
+
+        public override string ToString()
+        {
+            return $"Drawing {WhatToRenderAs} as lines";
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public string WhatToRenderAs { get; set; }
+
+        public override string ToString()
+        {
+            return $"Drawing {WhatToRenderAs} as pixels";
+        }
+    }
+}
