c1e7edbabeee79d3f67409b35e7edf7ff16a8fbf
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2019-05-18 16:43:47 -0300

    adding new module Adapter Pattern

---
+++ DesignPatters-6-Adapter/Example1/Adapter.cs
@@ -0,0 +1,117 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using static System.Console;
+
+namespace DesignPatters_6_Adapter.Example1
+{
+    public class AdapterExample
+    {
+        static void Mains(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Draw();
+            Draw();
+        }
+
+        private static void Draw()
+        {
+            foreach (var vo in vectorObjects)
+            {
+                foreach (var line in vo)
+                {
+                    var adapter = new LineToPointAdapter(line);
+                    adapter.ForEach(DrawPoint);
+                }
+            }
+        }
+
+        public static void DrawPoint(Point p)
+        {
+            Write(".");
+        }
+
+        private static readonly List<VectorObject> vectorObjects
+            = new List<VectorObject>()
+            {
+                new VectorRectangle(1, 1, 10, 10),
+                new VectorRectangle(3, 3, 6, 6)
+            };
+    }
+
+    public class LineToPointAdapter : Collection<Point>
+    {
+        private static int count;
+
+        public LineToPointAdapter(Line line)
+        {
+            WriteLine($"{++count}: Generating points for line [{line.Start.X}, {line.Start.Y}]-[{line.End.X}, {line.End.Y}]");
+
+            int left = Math.Min(line.Start.X, line.End.X);
+            int right = Math.Max(line.Start.X, line.End.X);
+            int top = Math.Min(line.Start.Y, line.End.Y);
+            int bottom = Math.Max(line.Start.Y, line.End.Y);
+            int dx = right - left;
+            int dy = line.End.Y - line.Start.Y;
+
+            if (dx == 0)
+            {
+                for (int y = top; y <= bottom; ++y)
+                {
+                    Add(new Point(left, y));
+                }
+            }
+            else if (dy == 0)
+            {
+                for (int x = left; x <= right; ++x)
+                {
+                    Add(new Point(x, top));
+                }
+            }
+        }
+    }
+
+    public class Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line(Point start, Point end)
+        {
+            if (start == null)
+                throw new ArgumentNullException(paramName: nameof(start));
+            if (end == null)
+                throw new ArgumentNullException(paramName: nameof(end));
+
+            Start = start;
+            End = end;
+        }
+    }
+
+    public class VectorObject : Collection<Line>
+    {
+
+    }
+
+    public class VectorRectangle : VectorObject
+    {
+        public VectorRectangle(int x, int y, int width, int height)
+        {
+            Add(new Line(new Point(x, y), new Point(x + width, y)));
+            Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+            Add(new Line(new Point(x, y), new Point(x, y + height)));
+            Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+        }
+    }
+}
---
+++ DesignPatters-6-Adapter/Exercise/Exercise.cs
@@ -0,0 +1,53 @@
+﻿using static System.Console;
+
+namespace DesignPatters_6_Adapter.Exercise
+{
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        private readonly Square square;
+
+        public SquareToRectangleAdapter(Square square)
+        {
+            this.square = square;
+        }
+
+        public int Width => square.Side;
+        public int Height => square.Side;
+    }
+
+    public class Exercise
+    {
+        static void Main()
+        {
+            var square = new Square
+            {
+                Side = 10
+            };
+
+            IRectangle rectangle = new SquareToRectangleAdapter(square);
+
+            var area = rectangle.Area();
+
+            Write($"The area of the Rectangle is: {area}");
+        }
+    }
+}
