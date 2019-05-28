57572fb3b898f500a43eb837123b58f9a4e7cafb
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2019-05-27 21:56:01 -0300

    adding new module 'Composite'

---
+++ DesignPatters-8-Composite/Example1/CompositeExample.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace DesignPatters_8_Composite.Example1
+{
+    public class CompositeExample
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
