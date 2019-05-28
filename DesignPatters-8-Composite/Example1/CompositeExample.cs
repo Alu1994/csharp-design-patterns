3fe37b28fba22422d79f3164d0a71857ec20983f
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2019-05-27 23:24:11 -0300

    Adding a part of composite
    Esse padrao tem por missao gerar objetos de sua base ou listas de objetos de sua base, seguindo o mesmo padrão

--- DesignPatters-8-Composite/Example1/CompositeExample.cs
+++ DesignPatters-8-Composite/Example1/CompositeExample.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using static System.Console;
 
 namespace DesignPatters_8_Composite.Example1
@@ -10,7 +11,54 @@ namespace DesignPatters_8_Composite.Example1
     {
         static void Main(string[] args)
         {
-            
+            var drawing = new GraphicObject {Name = "My Drawing"};
+            drawing.Children.Add(new Square {Color = "Red"});
+            drawing.Children.Add(new Circle {Color = "Yellow"});
+
+            var group = new GraphicObject();
+            group.Children.Add(new Circle {Color = "Blue"});
+            group.Children.Add(new Square {Color = "Blue"});
+            drawing.Children.Add(group);
+
+            WriteLine(drawing);
         }
+    }
+
+    public class GraphicObject
+    {
+        public virtual string Name { get; set; } = "Group";
+        public string Color;
+
+        private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>();
+        public List<GraphicObject> Children => children.Value;
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+                .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ")
+                .AppendLine(Name);
+
+            foreach(var child in Children)
+            {
+                child.Print(sb, depth+1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+    }
+
+    public class Square : GraphicObject
+    {
+        public override string Name => "Square";
     }
 }
