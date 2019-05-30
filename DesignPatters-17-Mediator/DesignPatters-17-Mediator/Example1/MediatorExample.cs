082117687f238093aa87bdd8f9825a06bdebbd98
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2019-05-30 00:13:20 -0300

    :sparkles: Adding the full Decorator Pattern examples
    Adding the Mediator Project and Folder

---
+++ DesignPatters-17-Mediator/DesignPatters-17-Mediator/Example1/MediatorExample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatters_17_Mediator.Example1
+{
+    public class MediatorExample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DesignPatters-9-Decorator/DesignPatters-9-Decorator/Exercise/MyExercise.cs
@@ -0,0 +1,51 @@
+﻿using static System.Console;
+
+namespace DesignPatters_9_Decorator.Exercise
+{
+    public class MyExercise
+    {
+        static void Main(string[] args)
+        {
+            var d = new Dragon();
+            d.Age = 2;
+            WriteLine(d.Fly());
+            WriteLine(d.Crawl());
+        }
+    }
+
+    public class Bird
+    {
+        public int Age { get; set; }
+
+        public string Fly() => (Age < 10) ? "flying" : "too old";
+    }
+
+    public class Lizard
+    {
+        public int Age { get; set; }
+
+        public string Crawl() => (Age > 1) ? "crawling" : "too young";
+    }
+
+    public class Dragon // no need for interfaces
+    {
+        public Lizard lizard = new Lizard();
+        public Bird bird = new Bird();
+        private int age;
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                bird.Age = value;
+                lizard.Age = value;
+            }
+        }
+
+        public string Fly() => bird.Fly();
+
+        public string Crawl() => lizard.Crawl();
+    }
+}
