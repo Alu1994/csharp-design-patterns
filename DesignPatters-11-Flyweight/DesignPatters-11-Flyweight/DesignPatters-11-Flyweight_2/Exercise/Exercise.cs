4aa3db2efe9d22d2e6b250a16c65f51c1df1c3b8
Matheus Veiga <matheus.veiga@stone.com.br>
2019-06-25 21:51:22 -0300

    Updating file locations

--- DesignPatters-11-Flyweight/DesignPatters-11-Flyweight/DesignPatters-11-Flyweight_2/Exercise/Exercise.cs
+++ DesignPatters-11-Flyweight/DesignPatters-11-Flyweight/DesignPatters-11-Flyweight_2/Exercise/Exercise.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using static System.Console;
 
 namespace DesignPatters_11_Flyweight_2.Exercise
