a525495f21afd9d3a80a237ef1b57f97608531dc
Matheus Veiga <matheus.veiga@stone.com.br>
2019-06-25 21:45:26 -0300

    Adding Flyweight pattern

---
+++ DesignPatters-11-Flyweight/DesignPatters-11-Flyweight/DesignPatters-11-Flyweight_2/Exercise/Exercise.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace DesignPatters_11_Flyweight_2.Exercise
+{
+    public class TestIt
+    {
+        static void Main(string[] args)
+        {
+            var sentence = new Sentence("Hello World");
+            sentence[1].Capitalize = true;
+            WriteLine(sentence);
+        }
+    }
+
+    public class Sentence
+    {
+        private string[] words;
+        private Dictionary<int, WordToken> _tokens = new Dictionary<int, WordToken>();
+
+        public Sentence(string plainText)
+        {
+            words = plainText.Split(' ');
+        }
+
+        public WordToken this[int index]
+        {
+            get
+            {
+                WordToken wt = new WordToken();
+                _tokens.Add(index, wt);
+                return _tokens[index];
+            }
+        }
+
+        public override string ToString()
+        {
+            var ws = new List<string>();
+            for (var i = 0; i < words.Length; i++)
+            {
+                var w = words[i];
+                if (_tokens.ContainsKey(i) && _tokens[i].Capitalize)
+                    w = w.ToUpperInvariant();
+                ws.Add(w);
+            }
+            return string.Join(" ", ws);
+        }
+
+        public class WordToken
+        {
+            public bool Capitalize;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -137,6 +137,55 @@ faz necessária a sua exposição toda à todos os seus consumidores, mas sendo 
 
 ## Section: 11
 ### Flyweight
+- Quando um dado/objeto é replicado e você somente precisa da "referencia" dele, uma maneira de tratar esse caso seria usando o Pattern Flyweight.
+
+```
+	Ex:
+		using System.Collections.Generic;
+		using static System.Console;
+
+		namespace DesignPatters_11_Flyweight_2.Exercise
+		{
+			public class Sentence
+			{
+				private string[] words;
+				private Dictionary<int, WordToken> _tokens = new Dictionary<int, WordToken>();
+
+				public Sentence(string plainText)
+				{
+					words = plainText.Split(' ');
+				}
+
+				public WordToken this[int index]
+				{
+					get
+					{
+						WordToken wt = new WordToken();
+						_tokens.Add(index, wt);
+						return _tokens[index];
+					}
+				}
+
+				public override string ToString()
+				{
+					var ws = new List<string>();
+					for (var i = 0; i < words.Length; i++)
+					{
+						var w = words[i];
+						if (_tokens.ContainsKey(i) && _tokens[i].Capitalize)
+							w = w.ToUpperInvariant();
+						ws.Add(w);
+					}
+					return string.Join(" ", ws);
+				}
+
+				public class WordToken
+				{
+					public bool Capitalize;
+				}
+			}
+		}
+```
 
 ## Section: 12
 ### Proxy
